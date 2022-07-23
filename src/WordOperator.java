d664c00cf3304762a6c4bc25cbb42ee3159ee09a
hosseinmasoodi <hosseinmasoodiii@gmail.com>
2022-07-23 11:24:01 +0430

    Checkers added

--- src/WordOperator.java
+++ src/WordOperator.java
@@ -1,15 +1,25 @@
+import java.util.ArrayList;
+
 public class WordOperator {
     public static WordContainer operate(String[] wordsToFind) {
         WordContainer wordContainer = new WordContainer();
+        ArrayList<ConditionChecker> checkerList = init(wordContainer);
         for (String word : wordsToFind) {
-            if (word.startsWith("+")) {
-                wordContainer.plusWords.add(word.substring(1));
-            } else if (word.startsWith("-")) {
-                wordContainer.minusWords.add(word.substring(1));
-            } else {
-                wordContainer.normalWords.add(word);
+            for (ConditionChecker checker : checkerList) {
+                boolean flag = checker.checkAndAdd(word);
+                if (flag){
+                    break;
+                }
             }
         }
         return wordContainer;
+    }
+
+    private static ArrayList<ConditionChecker> init(WordContainer wordContainer) {
+        ArrayList<ConditionChecker> toReturn = new ArrayList<>();
+        toReturn.add(new PlusWordsChecker(wordContainer));
+        toReturn.add(new MinusWordsChecker(wordContainer));
+        toReturn.add(new NormalWordsChecker(wordContainer));
+        return toReturn;
     }
 }
