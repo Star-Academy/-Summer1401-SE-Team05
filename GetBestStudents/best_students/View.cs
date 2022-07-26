688606e3719c4d491aa52215e42c0d7d21654ac6
mojo-izadi <80407427+mojo-izadi@users.noreply.github.com>
2022-07-26 18:30:58 +0430

    refactor add to ignore

--- GetBestStudents/best_students/View.cs
+++
@@ -1,9 +0,0 @@
-namespace BEST_STUDENTS;
-
-public class View
-{
-    public void PrintList<T>(IEnumerable<T> enumerable)
-    {
-        foreach(var item in enumerable) Console.WriteLine(item.ToString());
-    }
-}
---
+++ src/CheckersAndOperators.java
@@ -0,0 +1,6 @@
+public class CheckersAndOperators {
+    FileReader fileReader = new FileReader();
+    DocumentChecker documentChecker = new DocumentChecker();
+    IOOperations ioOperations = new IOOperations();
+    WordOperator wordOperator = new WordOperator();
+}
