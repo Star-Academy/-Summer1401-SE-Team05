4330f3728714ee6c62dc56698069d6e2c2900d7b
mojo-izadi <80407427+mojo-izadi@users.noreply.github.com>
2022-08-09 12:13:55 +0430

    implementing runnable

--- phase5/Solution1/ConsoleApp1/Runnable.cs
+++ phase5/Solution1/ConsoleApp1/Runnable.cs
@@ -3,19 +3,27 @@ namespace ConsoleApp1;
 public class Runnable
 {
     private ReaderWriter _readerWriter;
+    private FileReader _fileReader;
 
     public Runnable(ReaderWriter readerWriter)
     {
         _readerWriter = readerWriter;
     }
 
-    public void GetPathFromUser()
+    private void GetPathFromUser()
     {
         _readerWriter.WriteLine("please enter the path of your resources");
-        _readerWriter.ReadLine();
+        var path = _readerWriter.ReadLine();
+        _fileReader = new FileReader(path);
     }
 
     public void Run()
+    {
+        GetPathFromUser();
+        getQueryFromUser();
+    }
+
+    private void getQueryFromUser()
     {
         
     }
