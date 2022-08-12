c2205d9e164a77fdee5a32a67156f883a5b502ba
mojo-izadi <80407427+mojo-izadi@users.noreply.github.com>
2022-08-12 19:32:29 +0430

    making private

--- phase5/SampleLibrary.Test/MinusWordHandlerTest.cs
+++ phase5/SampleLibrary.Test/MinusWordHandlerTest.cs
@@ -4,11 +4,11 @@ namespace SampleLibrary.Test;
 
 public class MinusWordHandlerTest
 {
-    public readonly List<string> AllWords = new(new [] {"+this", "-is", "+a", "test", "string", "-for", "+word", "bank"});
-    public readonly List<string> AllWordsMinus = new(new [] {"is", "for"});
-    public readonly List<string> AllWordsNotMinus = new(new [] {"+this", "+a", "test", "string", "+word", "bank"});
+    private readonly List<string> _allWords = new(new [] {"+this", "-is", "+a", "test", "string", "-for", "+word", "bank"});
+    private readonly List<string> _allWordsMinus = new(new [] {"is", "for"});
+    private readonly List<string> _allWordsNotMinus = new(new [] {"+this", "+a", "test", "string", "+word", "bank"});
 
-    public readonly List<string> RegWords = new(new [] {"this", "is", "a", "test", "string", "for", "word", "bank"});
+    private readonly List<string> _regWords = new(new [] {"this", "is", "a", "test", "string", "for", "word", "bank"});
 
     
     [Fact]
@@ -18,11 +18,11 @@ public class MinusWordHandlerTest
         var handler = new MinusWordSeperator();
         
         //act
-        var result = handler.Separate(AllWords);
+        var result = handler.Separate(_allWords);
         
         //assert
-        handler.Words.Should().Equal(AllWordsMinus);
-        result.Should().Equal(AllWordsNotMinus);
+        handler.Words.Should().Equal(_allWordsMinus);
+        result.Should().Equal(_allWordsNotMinus);
 
     }
     
@@ -33,11 +33,11 @@ public class MinusWordHandlerTest
         var handler = new MinusWordSeperator();
 
         //act
-        var result = handler.Separate(RegWords);
+        var result = handler.Separate(_regWords);
         
         //assert
         handler.Words.Should().BeEmpty();
-        result.Should().Equal(RegWords);
+        result.Should().Equal(_regWords);
 
     }
 
